dfa0795f5f5ffd7417d23ef467125122181f5c0b
Kendall Burdette <kjburdette2015@gmail.com>
2021-03-18 20:05:11 -0400

    Co-authored-by: Gill Guimaraes <TechNestOwl@users.noreply.github.com> Co-authored-by: Michael Bulostin <mjbulostin@users.noreply.github.com>

--- rpgClass.py
+++ rpgClass.py
@@ -1,24 +1,56 @@
-class Characters:
+class Hero:
     def __init__(self, name, hp, defense, attack):
         self.name = name
         self.hp = hp
         self.defense = defense
         self.attack = attack
-
-
-class Hero(Characters):
-    def coinTransaction(self, coinPurse):
-        self.coinPurse = coinPurse
-
+        self.characterClass = "Hero"
+    
+# class Hero(Characters):
+#     pass
+    # def coinTransaction(self, coinPurse, coin):
+    #     self.coinPurse = coinPurse
+    #     self.coin = coin
+    #     coinPurse += coin
+    #     print(f"{coin} coin's have been added to your coin purse")
+#^^^ needs revision ^^^
 
 # Heros
-heroHuman = Hero("Human", 100, 50, 25)
-heroOrc = Hero("Orc", 100, 25, 75)
-heroElf = Hero("Elf", 100, 75, 25)
 
 # Villans
-villanMinion = Characters("Minion", 100, 20, 20)
-villanBoss = Characters("Boss", 100, 75, 75)
+# villainMinion = Characters("Minion", 100, 20, 20)
+# villainBoss = Characters("Boss", 100, 75, 75)
 
-print(heroHuman.name)
-print(villanBoss.name)
+# print(heroHuman.name)
+# print(villanBoss.name)
+def selectChar():
+    # charName = input("What is your name?")
+    character = ""
+    while character == "":
+        choice = input("""Please select a character
+        h. Human
+        o. Orc
+        e. Elf
+        """)
+        if ( choice == "h"):
+            character = Hero("Human", 100, 50, 25)
+        elif ( choice == "o"):
+            character = Hero("Orc", 100, 25, 75)  
+        elif ( choice == "e"):
+            character = Hero("Elf", 100, 25, 50)
+        else:
+            print("Please follow my rules and enter a valid choice.")
+    return character
+
+
+
+character = selectChar()
+print(f"""
+        Your class is: {character.name} \n
+        Health: {character.hp} \n
+        Defense: {character.defense} \n
+        Attack: {character.attack} \n
+    """)
+
+# print(character.name)
+
