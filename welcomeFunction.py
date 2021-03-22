40140ac5ce6a753db26f458e4029a950292626eb
frankie ferbrache <francisco.ferbrache@gmail.com>
2021-03-21 21:51:03 -0400

    added gitignore

--- welcomeFunction.py
+++ welcomeFunction.py
@@ -3,16 +3,23 @@ def welcomeMessage():
                 ________________________
               <|     PYTHON $LAYER      |>
               <|      Created By        |>
-              <|     ATLients,Inc.      |>
+              <|     ATLiens,Inc.      |>
                 `-----------------------`
-                            
+
+                                       .|
+                       | |
+                       |'|            ._____
+               ___    |  |            |.   |' .---"|
+       _    .-'   '-. |  |     .--'|  ||   | _|    |
+    .-'|  _.|  |    ||   '-__  |   |  |    ||      |
+    |' | |.    |    ||       | |   |  |    ||      |
+ ___|  '-'     '    ""       '-'   '-.'    '`      |____
+        
 
 Greetings, hero! We need your help slaying the almighty Python who has
-commandeered our village townhall! Others have attempted in the past, and 
-those who fled and survived have notified us that there are snake handlers 
-who have been brainswashed and are guarding the Python. Should you defeat 
-the minions, they will drop coins which will allow you to purchase a better
+commandeered the ATL Tech Village! Others have attempted in the past, and 
+those who fled and survived have notified us that there are corrupted developers
+who have been brainwashed and are guarding the Python. Should you defeat 
+them, they will drop rubber duckies which will allow you to purchase a better
 weapon at the barracks to aid you against the Python boss!
-
-
-      """)
+""")
